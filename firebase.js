778a41922227647cea05ee270299bda07a490eef
chiragdhunna <chiragdhunna2468@gmail.com>
2025-04-16 14:02:27 +0530

    fix: update firebase configuration to match project settings

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCIn_yQge4J1mr-sR_6ErI5ZFhalY7uzM",
-    authDomain: "react-native-uber-eats-c-47ab4.firebaseapp.com",
-    projectId: "react-native-uber-eats-c-47ab4",
-    storageBucket: "react-native-uber-eats-c-47ab4.appspot.com",
-    messagingSenderId: "705228949591",
-    appId: "1:705228949591:web:3d546097947590daf63b12"
-  };
+  apiKey: "AIzaSyAuQN-pnptR-SDSItfxIozAFOt42TYnPfM",
+  authDomain: "uber-eats-clone-16f71.firebaseapp.com",
+  projectId: "uber-eats-clone-16f71",
+  storageBucket: "uber-eats-clone-16f71.firebasestorage.app",
+  messagingSenderId: "471625841737",
+  appId: "1:471625841737:android:604613a3e5062dd8a65398",
+};
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
